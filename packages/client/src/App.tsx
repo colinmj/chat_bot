4babe78ca6761a38df638b254cb51f62a03ea9a2
Matson-Jones <Colin.Matson-Jones@MAC2NDHHV.local>
2026-03-17 06:09:50 -0700

    Finished backend

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { Button } from "./components/ui/button";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <>
+      <p className="font-bold">{message}</p>
+      <Button>Click ME!</Button>
+    </>
+  );
+}
+
+export default App;
